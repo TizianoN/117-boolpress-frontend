fb7d074ce8a3fc0dde179ae14152acf57968902c
TizianoN <hello@tizianonicolai.com>
2024-04-17 16:43:24 +0200

    fix includes

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const api = {
-  baseUrl: document.location.includes('digitalocean') ? process.env.API_URL : 'http://127.0.0.1:8000/api/',
+  baseUrl: window.location.toString().includes('digitalocean') ? process.env.API_URL : 'http://127.0.0.1:8000/api/',
 };
 
 export const store = reactive({
