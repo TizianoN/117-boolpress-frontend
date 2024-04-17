3487f896c8d59d27e3edc4cbbc753dbd01028253
TizianoN <hello@tizianonicolai.com>
2024-04-17 19:49:49 +0200

    fix api url

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { reactive } from 'vue';
 
 export const api = {
-  baseUrl: process.env.APP_FRONTOFFICE_URL ? process.env.APP_FRONTOFFICE_URL + '/api/' : 'http://localhost:8000/api/',
+  baseUrl: window.location.toString().includes('boolpress')
+    ? 'https://boolpress-admin.tizianonicolai.com/api/'
+    : 'http://localhost:8000/api/',
 };
 
 export const store = reactive({
