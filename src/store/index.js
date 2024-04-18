87e2e1c4c891a211b78488f3321fa5437adfc5d9
TizianoN <hello@tizianonicolai.com>
2024-04-18 09:15:43 +0200

    back to unpublished

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import { reactive } from 'vue';
 
 export const api = {
-  baseUrl: window.location.toString().includes('boolpress')
-    ? 'https://boolpress-admin-test.tizianonicolai.com/api/'
-    : 'http://localhost:8000/api/',
+  baseUrl: 'http://localhost:8000/api/',
 };
 
 export const store = reactive({
